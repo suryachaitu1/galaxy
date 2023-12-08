42b39741eddf8e2130306fa7ab0024b93e998431
srisai <srisai@Chaitanya>
2023-12-08 15:28:31 +0530

    2nd update

---
+++ src/test/java/program/Third.java
@@ -0,0 +1,19 @@
+package program;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Third {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
